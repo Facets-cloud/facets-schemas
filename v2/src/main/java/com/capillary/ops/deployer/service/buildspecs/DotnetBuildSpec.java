e813ed09d85db80aa4487030675bd41683e04294
Rohit <rohit.raveendran@capillarytech.com>
2019-04-16 14:12:41 +0530

    add proper repo name

--- v2/src/main/java/com/capillary/ops/deployer/service/buildspecs/DotnetBuildSpec.java
+++ v2/src/main/java/com/capillary/ops/deployer/service/buildspecs/DotnetBuildSpec.java
@@ -36,7 +36,7 @@ public class DotnetBuildSpec extends BuildSpec {
         List<String> preBuildCommands = new ArrayList<>();
         preBuildCommands.add("TAG=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | head -c 7)");
         preBuildCommands.add("REPO=" + ECR_REPO);
-        preBuildCommands.add("APP_NAME=" + application.getName());
+        preBuildCommands.add("APP_NAME=" + application.getApplicationFamily().name() + "/" + application.getName());
         return preBuildCommands;
     }
 
