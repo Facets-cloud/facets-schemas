04f1c894dde2de60469d0be7e1f1bee50e3aa213
Ambar Mehrotra <ambar.mehrotra@capillarytech.com>
2019-05-02 16:06:05 +0530

    - Added health check and corresponding script - Added corresponding liveness probe - Added json logging

--- v2/scripts/scheduled_s3_uploader/src/scheduled_s3_uploader.py
+++ v2/scripts/scheduled_s3_uploader/src/scheduled_s3_uploader.py
@@ -8,9 +8,14 @@ import sys
 import time
 
 from botocore.exceptions import ClientError
-
+from pythonjsonlogger import jsonlogger
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger()
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
 
 BASE_DUMP_DIR = os.environ.get("DUMP_DIR", "/var/log/dumps")
 S3_BUCKET = os.environ.get("S3_BUCKET", "k8s-file-dumps")
@@ -18,7 +23,7 @@ DEFAULT_FREQUENCY_MIN = 5
 
 CLUSTER = os.environ.get("CLUSTER")
 if CLUSTER is None:
-    logging.error("cluster cannot be absent")
+    logger.error("cluster cannot be absent")
     sys.exit(1)
 
 s3_client = boto3.client('s3')
@@ -39,15 +44,15 @@ def file_age_in_minutes(pathname):
 
 def upload_file_to_s3(module, path):
     object_name = CLUSTER + "/" + module + "/" + get_current_hour_folder() + "/" + path.split("/")[-1]
-    logging.info("writing file to path: {}".format(object_name))
+    logger.info("writing file to path: {}".format(object_name))
     try:
         response = s3_client.upload_file(path, S3_BUCKET, object_name)
-        logging.info("uploaded file successfully: {}".format(object_name))
+        logger.info("uploaded file successfully: {}".format(object_name))
     except ClientError as e:
-        logging.error(e)
+        logger.error(e)
         return False
     return True
-    
+
 
 def filter_files_in_dir(dirpath):
     """
@@ -60,40 +65,48 @@ def filter_files_in_dir(dirpath):
 
 def upload_module(module, path):
     # Filter the contents for files only
-    logging.info("finding files to upload in the path: {}".format(path))
+    logger.info("finding files to upload in the path: {}".format(path))
     files = filter_files_in_dir(path)
-    logging.info("going to upload following files: {}".format(files))
+    logger.info("going to upload following files: {}".format(files))
     for file in files:
         filepath = path + "/" + file
         # Only upload file if the file was modified more than 5 minutes ago
         if not (file_age_in_minutes(filepath) > 5):
-            logging.info("file less than 5 minutes old: {}".format(filepath))
+            logger.info("file less than 5 minutes old: {}".format(filepath))
             continue
         # Delete the file if the upload was successful
         is_upload_successful = upload_file_to_s3(module, filepath)
         if is_upload_successful:
-            logging.info("removing file: {}".format(filepath))
+            logger.info("removing file: {}".format(filepath))
             os.unlink(filepath)
 
 
 def schedule_module_upload():
     # Clear all tasks tagged as 5-min-task
     schedule.clear('5-min-task')
-    logging.info("cleared all 5 min tasks")
+    logger.info("cleared all 5 min tasks")
     for module in os.listdir(BASE_DUMP_DIR):
         path = BASE_DUMP_DIR + "/" + module
         # Add a schedule recurring every 5 mins for all directoris/modules
         if os.path.isdir(path):
-            logging.info("scheduling 5 min task for module: {}".format(module))
+            logger.info("scheduling 5 min task for module: {}".format(module))
             upload_module(module, path)
             schedule.every(DEFAULT_FREQUENCY_MIN).minutes.do(upload_module, module, path).tag("5-min-task")
+
+
+def health_check():
+    current_timestamp = round(time.time())
+    with open('/tmp/healthy', 'a+') as fout:
+        fout.write('{} - healthy\n'.format(current_timestamp))
 
 
 schedule_module_upload()
 
 # Check for any new modules every 15 minutes and refresh the list of schedules
 schedule.every(15).minutes.do(schedule_module_upload).tag("15-min-task")
-logging.debug("scheduled task for refreshing module list")
+logger.debug("scheduled task for refreshing module list")
+schedule.every(5).seconds.do(health_check).tag("health-check")
+logger.debug("scheduled health check to run every 5 seconds")
 
 while True:
     schedule.run_pending()
