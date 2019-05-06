28c6da71b07ab4d0ba745d2a7a7f39e9125c52b5
Rohit <rohit.raveendran@capillarytech.com>
2019-05-06 21:08:50 +0530

    complete google auth

--- v2/src/main/java/com/capillary/ops/deployer/SecurityConfig.java
+++ v2/src/main/java/com/capillary/ops/deployer/SecurityConfig.java
@@ -1,35 +1,44 @@
 package com.capillary.ops.deployer;
 
-import com.capillary.ops.deployer.service.UserAuthorizationService;
+import com.capillary.ops.deployer.service.OAuth2UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
 
 @Configuration
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
   @Autowired
-  private UserAuthorizationService userAuthorizationService;
+  private OAuth2UserServiceImpl oAuth2UserService;
 
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userAuthorizationService);
   }
 
   protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic()
-            .and()
-            .authorizeRequests()
+    http.authorizeRequests()
             .anyRequest()
             .authenticated()
-            .anyRequest()
-            .hasRole("ADMIN")
             .and()
-            .csrf()
-            .disable()
-            .headers()
-            .frameOptions()
-            .disable();
+            .oauth2Login()
+            .userInfoEndpoint()
+            .userService(oAuth2UserService)
+            .and()
+            .and()
+            .csrf().disable();
   }
 }
