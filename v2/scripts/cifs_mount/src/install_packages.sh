cd417c1c667dcca1c59bfaba8505aebb669402e5
Rohit <rohit.raveendran@capillarytech.com>
2019-06-03 18:22:45 +0530

    fix dotnet image

--- v2/scripts/cifs_mount/src/install_packages.sh
+++ v2/scripts/cifs_mount/src/install_packages.sh
@@ -7,6 +7,6 @@ apt-get update && apt-get install -y -o Apt::Get::AllowUnauthenticated=true smbc
 echo -e "\nInstalling cifs-utils..."
 apt-get update && apt-get install -y cifs-utils
 
-apt-get update && apt-get install dnsutils -y
+apt-get update && apt-get install tzdata dnsutils -y
 
 wget -q https://download.newrelic.com/dot_net_agent/latest_release/newrelic-netcore20-agent_8.15.455.0_amd64.deb && dpkg -i newrelic-netcore20-agent_8.15.455.0_amd64.deb
