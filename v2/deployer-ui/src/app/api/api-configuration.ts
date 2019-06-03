6a7f074db30714c1ab77f2852b2c3f604fc36d02
Rohit <rohit.raveendran@capillarytech.com>
2019-06-04 01:27:00 +0530

    ui changes

--- v2/deployer-ui/src/app/api/api-configuration.ts
+++ v2/deployer-ui/src/app/api/api-configuration.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = '//deployer.capillary.in';
+  rootUrl: string = '//localhost:8100';
 }
 
 export interface ApiConfigurationInterface {
