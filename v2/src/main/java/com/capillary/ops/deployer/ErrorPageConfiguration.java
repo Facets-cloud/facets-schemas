cf4a1c819d0a9e15ec45896fdbbf0a008779d758
Rohit <rohit.raveendran@capillarytech.com>
2019-06-14 02:53:39 +0530

    disable cache for html

--- v2/src/main/java/com/capillary/ops/deployer/ErrorPageConfiguration.java
+++ v2/src/main/java/com/capillary/ops/deployer/ErrorPageConfiguration.java
@@ -5,13 +5,16 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class ErrorPageConfiguration implements WebMvcConfigurer {
+
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -29,4 +32,10 @@ public class ErrorPageConfiguration implements WebMvcConfigurer {
         };
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/index.html")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noCache());
+    }
 }
