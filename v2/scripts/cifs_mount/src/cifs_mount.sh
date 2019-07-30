b56f2e20a58830501c5a80be425570109c4d3e42
Rohit <rohit.raveendran@capillarytech.com>
2019-07-30 17:09:22 +0530

    escape space

--- v2/scripts/cifs_mount/src/cifs_mount.sh
+++ v2/scripts/cifs_mount/src/cifs_mount.sh
@@ -13,7 +13,7 @@ else
 fi
 if [[ "$ENABLE_NEWRELIC" = "true" ]] ; then
     echo "Enabling newrelic"
-    sed -i s/My Application/${CLUSTER}-${NEW_RELIC_APP_NAME}/g /usr/local/newrelic-netcore20-agent/newrelic.config
+    sed -i s/My\ Application/${CLUSTER}-${NEW_RELIC_APP_NAME}/g /usr/local/newrelic-netcore20-agent/newrelic.config
     sed -i s/REPLACE_WITH_LICENSE_KEY/$NEWRELIC_LICENSE_KEY/g /usr/local/newrelic-netcore20-agent/newrelic.config
 else
     echo "Newrelic is disabled"
