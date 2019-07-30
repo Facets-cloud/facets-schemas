72cfcbf17cbaac9e7fafd62fb0a49bf824a484d7
Rohit <rohit.raveendran@capillarytech.com>
2019-07-30 16:31:44 +0530

    add newrelic

--- v2/scripts/cifs_mount/src/cifs_mount.sh
+++ v2/scripts/cifs_mount/src/cifs_mount.sh
@@ -11,6 +11,12 @@ then
 else
     echo "MOUNT_CIFS was false, not mounting"
 fi
-
+if [[ "$ENABLE_NEWRELIC" = "true" ]] ; then
+    echo "Enabling newrelic"
+    sed -i s/My Application/${CLUSTER}-${NEW_RELIC_APP_NAME}/g /usr/local/newrelic-netcore20-agent/newrelic.config
+    sed -i s/REPLACE_WITH_LICENSE_KEY/$NEWRELIC_LICENSE_KEY/g /usr/local/newrelic-netcore20-agent/newrelic.config
+else
+    echo "Newrelic is disabled"
+fi
 echo $@
 $@
