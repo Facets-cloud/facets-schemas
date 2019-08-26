c736573e401558d0c890d9357811251474f10e57
Rohit <rohit.raveendran@capillarytech.com>
2019-08-26 16:11:50 +0530

    fix

--- v2/src/main/java/com/capillary/ops/deployer/service/buildspecs/MJNugetBuildSpec.java
+++ v2/src/main/java/com/capillary/ops/deployer/service/buildspecs/MJNugetBuildSpec.java
@@ -16,7 +16,7 @@ public class MJNugetBuildSpec extends BuildSpec {
     @Override
     protected List<String> getPostBuildCommands() {
         List<String> postBuildCommands = new ArrayList<>();
-        postBuildCommands.add("- nuget push *.nupkg -Source mj-snapshot");
+        postBuildCommands.add("nuget push *.nupkg -Source mj-snapshot");
         return postBuildCommands;
     }
 
@@ -24,7 +24,7 @@ public class MJNugetBuildSpec extends BuildSpec {
     protected List<String> getBuildCommands() {
         ArrayList<String> buildCommands = new ArrayList<>();
         long version = System.currentTimeMillis();
-        buildCommands.add(" - '& nuget pack *.nuspec -Version $version-" + version);
+        buildCommands.add("nuget pack ($(Get-ChildItem -Filter \"*.nuspec\")[0]).Name -Version $version");
         // -alpha-$env:CODEBUILD_SOURCE_VERSION'
         return buildCommands;
     }
@@ -32,8 +32,8 @@ public class MJNugetBuildSpec extends BuildSpec {
     @Override
     protected List<String> getPreBuildCommands() {
         List<String> preBuildCommands = new ArrayList<>();
-        preBuildCommands.add("- $version = Get-Content version");
-
+        preBuildCommands.add("$version = Get-Content version");
+        preBuildCommands.add("$version = $version + '-alpha-' + [int64](([datetime]::UtcNow)-(get-date \"1/1/1970\")).TotalMilliseconds");
         return preBuildCommands;
     }
 
