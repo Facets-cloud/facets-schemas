a80b893c94a5e28bf5192c09723551c1f7056bb6
Ambar Mehrotra <ambar.mehrotra@capillarytech.com>
2019-08-26 15:36:32 +0530

    added loggers

--- v2/src/main/java/com/capillary/ops/deployer/service/impl/BitbucketVcsService.java
+++ v2/src/main/java/com/capillary/ops/deployer/service/impl/BitbucketVcsService.java
@@ -1,5 +1,6 @@
 package com.capillary.ops.deployer.service.impl;
 
+import com.capillary.ops.deployer.exceptions.NotFoundException;
 import com.capillary.ops.deployer.service.VcsService;
 import org.apache.commons.codec.Charsets;
 import org.apache.http.Header;
@@ -14,6 +15,9 @@ import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -26,18 +30,21 @@ import java.util.stream.Collectors;
 
 public class BitbucketVcsService implements VcsService {
 
+    private static final Logger logger = LoggerFactory.getLogger(BitbucketVcsService.class);
+
     private String baseUri = "https://api.bitbucket.org/2.0";
 
     private CloseableHttpClient getGETClient() {
         return HttpClients.custom().build();
     }
 
-    private void getPaginatedResponse(List<JSONObject> jsonValues, String requestUri) throws IOException {
-        String username = System.getenv("BITBUCKET_USERNAME");
-        String password = System.getenv("BITBUCKET_PASSWORD");
+    private void getPaginatedResponse(List<JSONObject> jsonValues, String requestUri, String username, String password) throws IOException {
+
         CloseableHttpClient httpClient = this.getGETClient();
 
+        logger.debug("constructing base64 encoded credentials");
         String authHeader = "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
+
         Header authorization = new BasicHeader(HttpHeaders.AUTHORIZATION, authHeader);
         Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 
@@ -46,12 +53,15 @@ public class BitbucketVcsService implements VcsService {
                 .addHeader(authorization)
                 .setUri(requestUri)
                 .build();
+        logger.debug("building request with uri: {}", requestUri);
 
         CloseableHttpResponse httpResponse = httpClient.execute(request);
 
         HttpEntity entity = httpResponse.getEntity();
         Header encodingHeader = entity.getContentEncoding();
         Charset encoding = encodingHeader == null ? StandardCharsets.UTF_8 : Charsets.toCharset(encodingHeader.getValue());
+
+        logger.debug("converting http response to json string with encoding: {}", encoding);
         String json = EntityUtils.toString(entity, encoding);
 
         JSONObject responseJson = new JSONObject(json);
@@ -62,11 +72,13 @@ public class BitbucketVcsService implements VcsService {
             return;
         }
 
-        getPaginatedResponse(jsonValues, responseJson.getString("next"));
+        getPaginatedResponse(jsonValues, responseJson.getString("next"), username, password);
+        httpResponse.close();
     }
 
     private void addPaginatedValuesToList(List<JSONObject> jsonValues, JSONArray values) {
         if (values.length() > 0) {
+            logger.debug("page contains values, adding to list");
             for (Object object: values) {
                 jsonValues.add((JSONObject) object);
             }
@@ -75,6 +87,14 @@ public class BitbucketVcsService implements VcsService {
 
     @Override
     public List<String> getBranches(String owner, String repository) throws IOException {
+        String username = System.getenv("BITBUCKET_USERNAME");
+        String password = System.getenv("BITBUCKET_PASSWORD");
+
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            logger.error("bitbucket credentials not found, please fulfill the credentials to use this api");
+            throw new NotFoundException("Please fulfill the Bitbucket credentials to use this api");
+        }
+
         CloseableHttpClient httpClient = this.getGETClient();
         String requestUri = new StringJoiner("/")
                 .add(this.baseUri)
@@ -86,7 +106,7 @@ public class BitbucketVcsService implements VcsService {
                 .toString();
 
         List<JSONObject> jsonValues = new ArrayList<>();
-        this.getPaginatedResponse(jsonValues, requestUri);
+        this.getPaginatedResponse(jsonValues, requestUri, username, password);
 
         List<String> branchList = jsonValues.parallelStream().map(x -> x.getString("name")).collect(Collectors.toList());
         httpClient.close();
@@ -96,6 +116,14 @@ public class BitbucketVcsService implements VcsService {
 
     @Override
     public List<String> getTags(String owner, String repository) throws IOException {
+        String username = System.getenv("BITBUCKET_USERNAME");
+        String password = System.getenv("BITBUCKET_PASSWORD");
+
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            logger.error("bitbucket credentials not found, please fulfill the credentials to use this api");
+            throw new NotFoundException("Please fulfill the Bitbucket credentials to use this api");
+        }
+
         CloseableHttpClient httpClient = this.getGETClient();
         String requestUri = new StringJoiner("/")
                 .add(this.baseUri)
@@ -107,7 +135,7 @@ public class BitbucketVcsService implements VcsService {
                 .toString();
 
         List<JSONObject> jsonValues = new ArrayList<>();
-        this.getPaginatedResponse(jsonValues, requestUri);
+        this.getPaginatedResponse(jsonValues, requestUri, username, password);
 
         List<String> tagList = jsonValues.parallelStream().map(x -> x.getString("name")).collect(Collectors.toList());
         httpClient.close();
--- v2/src/main/java/com/capillary/ops/deployer/service/impl/GithubVcsService.java
+++ v2/src/main/java/com/capillary/ops/deployer/service/impl/GithubVcsService.java
@@ -1,26 +1,40 @@
 package com.capillary.ops.deployer.service.impl;
 
+import com.capillary.ops.deployer.exceptions.NotFoundException;
 import com.capillary.ops.deployer.service.VcsService;
 import org.eclipse.egit.github.core.RepositoryBranch;
 import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.RepositoryTag;
 import org.eclipse.egit.github.core.service.RepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class GithubVcsService implements VcsService {
+
+    private static final Logger logger = LoggerFactory.getLogger(GithubVcsService.class);
+
     @Override
     public List<String> getBranches(String owner, String repository) throws IOException {
         String username = System.getenv("GIT_USERNAME");
         String password = System.getenv("GIT_PASSWORD");
 
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            logger.error("github credentials not found, please fulfill the credentials to use this api");
+            throw new NotFoundException("Please fulfill the Github credentials to use this api");
+        }
+
         RepositoryService repositoryService = new RepositoryService();
         repositoryService.getClient().setCredentials(username, password);
 
         RepositoryId repositoryId = new RepositoryId(owner, repository);
+        logger.debug("fetching github branches");
         List<RepositoryBranch> branches = repositoryService.getBranches(repositoryId);
+        logger.info("fetched {} branches from github for this application", branches.size());
 
         return branches.parallelStream().map(x -> x.getName()).collect(Collectors.toList());
     }
@@ -30,11 +44,18 @@ public class GithubVcsService implements VcsService {
         String username = System.getenv("GIT_USERNAME");
         String password = System.getenv("GIT_PASSWORD");
 
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            logger.error("github credentials not found, please fulfill the credentials to use this api");
+            throw new NotFoundException("Please fulfill the Github credentials to use this api");
+        }
+
         RepositoryService repositoryService = new RepositoryService();
         repositoryService.getClient().setCredentials(username, password);
 
         RepositoryId repositoryId = new RepositoryId(owner, repository);
+        logger.debug("fetching github tags");
         List<RepositoryTag> tags = repositoryService.getTags(repositoryId);
+        logger.info("fetched {} tags from github for this application", tags.size());
 
         return tags.parallelStream().map(x -> x.getName()).collect(Collectors.toList());
     }
