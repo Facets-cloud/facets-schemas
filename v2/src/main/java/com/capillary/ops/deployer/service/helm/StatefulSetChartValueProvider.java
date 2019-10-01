f9c32f9a2fd0e3a75089642585309bee4b55132f
Rohit <rohit.raveendran@capillarytech.com>
2019-10-01 14:40:59 +0530

    fix heirarchy

--- v2/src/main/java/com/capillary/ops/deployer/service/helm/StatefulSetChartValueProvider.java
+++ v2/src/main/java/com/capillary/ops/deployer/service/helm/StatefulSetChartValueProvider.java
@@ -3,6 +3,7 @@ package com.capillary.ops.deployer.service.helm;
 import com.capillary.ops.deployer.bo.Application;
 import com.capillary.ops.deployer.bo.Deployment;
 import com.capillary.ops.deployer.bo.Environment;
+import com.capillary.ops.deployer.service.helm.impl.AbstractValueProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class StatefulSetChartValueProvider extends BaseChartValueProvider {
+public class StatefulSetChartValueProvider extends AbstractValueProvider {
 
     @Override
     public Map<String, Object> getValues(Application application, Environment environment, Deployment deployment) {
