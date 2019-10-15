95060424c7f953715f653422bf6eacda0d706c8d
Madan Gudi <madan.gopal@capillarytech.com>
2019-10-15 18:00:44 +0530

    cap transfer logs chart

--- v2/src/main/java/com/capillary/ops/deployer/bo/K8sLoggingConfiguration.java
+++ v2/src/main/java/com/capillary/ops/deployer/bo/K8sLoggingConfiguration.java
@@ -2,6 +2,10 @@ package com.capillary.ops.deployer.bo;
 
 public class K8sLoggingConfiguration {
     String efsHost;
+    String awsAccessKeyID;
+    String awsSecretAccessKey;
+    String awsRegion;
+    String logsS3Bucket;
 
     public String getEfsHost() {
         return efsHost;
@@ -9,5 +13,37 @@ public class K8sLoggingConfiguration {
 
     public void setEfsHost(String efsHost) {
         this.efsHost = efsHost;
+    }
+
+    public String getAwsAccessKeyID() {
+        return awsAccessKeyID;
+    }
+
+    public void setAwsAccessKeyID(String awsAccessKeyID) {
+        this.awsAccessKeyID = awsAccessKeyID;
+    }
+
+    public String getAwsSecretAccessKey() {
+        return awsSecretAccessKey;
+    }
+
+    public void setAwsSecretAccessKey(String awsSecretAccessKey) {
+        this.awsSecretAccessKey = awsSecretAccessKey;
+    }
+
+    public String getAwsRegion() {
+        return awsRegion;
+    }
+
+    public void setAwsRegion(String awsRegion) {
+        this.awsRegion = awsRegion;
+    }
+
+    public String getLogsS3Bucket() {
+        return logsS3Bucket;
+    }
+
+    public void setLogsS3Bucket(String logsS3Bucket) {
+        this.logsS3Bucket = logsS3Bucket;
     }
 }
---
+++ v2/src/main/java/com/capillary/ops/deployer/bo/systemcharts/CapTransferLogsSystemChart.java
@@ -0,0 +1,42 @@
+package com.capillary.ops.deployer.bo.systemcharts;
+
+import com.capillary.ops.deployer.bo.ApplicationFamily;
+import com.capillary.ops.deployer.bo.Environment;
+import com.capillary.ops.deployer.bo.ISystemChart;
+import com.capillary.ops.deployer.bo.K8sLoggingConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CapTransferLogsSystemChart implements ISystemChart {
+
+    private static final Logger logger = LoggerFactory.getLogger(CapTransferLogsSystemChart.class);
+
+    @Override
+    public String getChartPath() {
+        return "cap-transfer-logs";
+    }
+
+    @Override
+    public Map<String, Object> getValues(ApplicationFamily applicationFamily, Environment environment) {
+        Map<String, Object> values = new HashMap<>();
+        K8sLoggingConfiguration logConfigs = environment.getEnvironmentConfiguration().getK8sLoggingConfiguration();
+        if(logConfigs == null) {
+            return null;
+        }
+        values.put("awsAccessKeyID",logConfigs.getAwsAccessKeyID());
+        values.put("awsSecretAccessKey",logConfigs.getAwsSecretAccessKey());
+        values.put("awsRegion",logConfigs.getAwsRegion());
+        values.put("logsS3Bucket",logConfigs.getLogsS3Bucket());
+        return values;
+    }
+
+    @Override
+    public String getReleaseName(ApplicationFamily applicationFamily, Environment environment) {
+        return "cap-transfer-logs";
+    }
+}
