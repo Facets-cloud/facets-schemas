b61c1389fb8a2d78b21114d022dac9902c5c5e92
Ambar Mehrotra <ambar.mehrotra@capillarytech.com>
2019-11-13 12:31:22 +0530

    corrected test report url and added pattern matching for application name

--- v2/src/main/java/com/capillary/ops/deployer/component/PullRequestScheduler.java
+++ v2/src/main/java/com/capillary/ops/deployer/component/PullRequestScheduler.java
@@ -40,7 +40,7 @@ public class PullRequestScheduler {
 
     private static final Logger logger = LoggerFactory.getLogger(PullRequestScheduler.class);
 
-    private static final String BASE_REPORTS_URL = "https://%s/api/%s/applications/%s/builds/%s";
+    private static final String BASE_REPORTS_URL = "https://%s/api/%s/applications/%s/tests/%s";
 
     private static final String DEFAULT_HOST = "deployer.capillary.in";
 
