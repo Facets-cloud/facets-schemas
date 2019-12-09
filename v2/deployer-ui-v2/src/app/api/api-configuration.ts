19f72ec9633aa6f97a53d4550cdf13660ccd2315
Rohit <rohit.raveendran@capillarytech.com>
2019-12-09 17:04:04 +0530

    fix backend url

--- v2/deployer-ui-v2/src/app/api/api-configuration.ts
+++ v2/deployer-ui-v2/src/app/api/api-configuration.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = '//localhost:4300';
+  rootUrl: string = '';
 }
 
 export interface ApiConfigurationInterface {
