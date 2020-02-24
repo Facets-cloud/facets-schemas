26a0a28727e9378f5d5d6f9a4d8b21804c2a150c
Rohit <rohit.raveendran@capillarytech.com>
2020-02-24 14:52:18 +0530

    mysql grants

---
+++ v2/src/main/resources/tfstandalone/application/mysql_credentials.tf
@@ -0,0 +1,42 @@
+locals {
+  mysql_credntial_requests = {for k in flatten([
+    for i,j in local.json_data["instances"]: [
+      for p in j["credentialRequests"]["dbs"]["mysql"]: {
+        mysql_host = var.resources["mysql"][p["resourceName"]]["original_endpoint"]
+        mysql_root_password = var.resources["mysql"][p["resourceName"]]["root_passowrd"]
+        mysql_grants = var.resources["mysql"][p["resourceName"]]["grants"][p["permission"]]
+        applicationName = i
+        key = "${i}-${p["resourceType"]}-${p["resourceName"]}-${p["permission"]}"
+        user = lower("${i}-${p["permission"]}")
+      }
+      ]]):
+    k.key => k
+  }
+}
+
+provider "mysql" {
+}
+
+resource "random_string" "random_password" {
+  for_each = local.mysql_credntial_requests
+  length = 12
+  override_special = "#$!"
+}
+
+resource "mysql_user" "mysql_user" {
+  for_each           = local.mysql_credntial_requests
+  user               = each.value["user"]
+  host               = "%"
+  override_endpoint  = each.value["mysql_host"]
+  plaintext_password = random_string.random_password[each.key].result
+  override_admin_username = "root"
+  override_admin_password = each.value["mysql_root_password"]
+}
+
+resource "mysql_grant" "mysql_grants" {
+  for_each           = local.mysql_credntial_requests
+  user       = mysql_user.mysql_user[each.key].user
+  host       = mysql_user.mysql_user[each.key].host
+  database   = "*"
+  privileges = each.value["mysql_grants"]
+}
--- v2/src/main/resources/tfstandalone/infra/mysql/outputs.tf
+++ v2/src/main/resources/tfstandalone/infra/mysql/outputs.tf
@@ -5,6 +5,10 @@ output "mysql_details" {
       original_endpoint: aws_db_instance.rds-instance[i].address
       k8s_endpoints: local.instances[i]["k8s_service_names"]
       root_passowrd: aws_db_instance.rds-instance[i].password
-    }
+      grants: {
+        READ_ONLY: ["SELECT"]
+        READ_WRITE: ["SELECT", "INSERT", "UPDATE"]
+      }
+  }
   }
 }
