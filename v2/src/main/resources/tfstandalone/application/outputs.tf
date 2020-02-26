dfed1e8057431f470239aa78b6d89707d0bc47a9
Rohit <rohit.raveendran@capillarytech.com>
2020-02-26 17:49:43 +0530

    debug

--- v2/src/main/resources/tfstandalone/application/outputs.tf
+++ v2/src/main/resources/tfstandalone/application/outputs.tf
@@ -1,7 +1,7 @@
 output "test" {
   value = {
     for i, j in local.json_data["instances"]:
-      i => merge(local.mysql_users[i], local.mysql_passwords[i])
+      i => local.mongo_passwords[i]
   }
 }
 
