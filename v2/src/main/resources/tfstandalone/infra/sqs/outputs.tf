bd77911b41c35d702798d970fc767a94b9ce33bc
Madan Gudi <madan.gopal@capillarytech.com>
2020-03-19 12:50:48 +0530

    sqs tf module fixes

--- v2/src/main/resources/tfstandalone/infra/sqs/outputs.tf
+++ v2/src/main/resources/tfstandalone/infra/sqs/outputs.tf
@@ -4,15 +4,9 @@ output "sqs_details" {
   i => {
     sqs_arn: aws_sqs_queue.sqs_queue[i].arn
     sqs_id: aws_sqs_queue.sqs_queue[i].id
+    sqs_consumer_policy = aws_iam_policy.consumer_policy.*.arn
+    sqs_producer_policy = aws_iam_policy.producer_policy.*.arn
   }
   }
-}
-
-output "sqs_consumer_policy" {
-  value       = aws_iam_policy.consumer_policy.*.arn
-}
-
-output "sqs_producer_policy" {
-  value       = aws_iam_policy.producer_policy.*.arn
 }
 
