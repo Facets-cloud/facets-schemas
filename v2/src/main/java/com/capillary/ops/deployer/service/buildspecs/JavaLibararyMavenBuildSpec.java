bd2daf3aec37e0f51e09e785a1c1f7c9afdfe9c7
Madan Gudi <madan.gopal@capillarytech.com>
2020-04-01 18:58:29 +0530

    java lib cache path fix

--- v2/src/main/java/com/capillary/ops/deployer/service/buildspecs/JavaLibararyMavenBuildSpec.java
+++ v2/src/main/java/com/capillary/ops/deployer/service/buildspecs/JavaLibararyMavenBuildSpec.java
@@ -47,6 +47,6 @@ public class JavaLibararyMavenBuildSpec extends MavenBuildSpec {
 
     @Override
     protected List<String> getCachePaths() {
-        return Arrays.asList("/root/.m2/repository/*");
+        return Arrays.asList("/root/.m2/repository/**/*");
     }
 }
