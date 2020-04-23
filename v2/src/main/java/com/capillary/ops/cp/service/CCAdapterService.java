621ddbb03da32609cefa9e93fce1e7e57df3fb31
anshulsao <anshul.sao@gmail.com>
2020-04-23 19:32:46 +0530

    ID Chekin

--- v2/src/main/java/com/capillary/ops/cp/service/CCAdapterService.java
+++ v2/src/main/java/com/capillary/ops/cp/service/CCAdapterService.java
@@ -29,7 +29,7 @@ public class CCAdapterService {
         return clustersByStackName.stream().map(c -> {
             EnvironmentMetaData environmentMetaData = new EnvironmentMetaData();
             environmentMetaData.setCapillaryCloudClusterName(c.getName());
-            environmentMetaData.setName(c.getName());
+            environmentMetaData.setName(c.getId());
             environmentMetaData.setApplicationFamily(applicationFamily);
             environmentMetaData.setCapCloud(true);
             environmentMetaData.setEnvironmentType(
