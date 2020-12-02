ee7e12c4cb90af8a1eb21b9a15a867a8db311a20
Anshul Sao <anshul.sao@capillarytech.com>
2020-12-02 17:58:46 +0530

    Auto complete for hotfix

--- v2/cc-ui/src/app/pages/cluster-pages/cluster-releases/cluster-releases.module.ts
+++ v2/cc-ui/src/app/pages/cluster-pages/cluster-releases/cluster-releases.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClusterReleasesComponent } from './cluster-releases.component';
-import {NbCardModule, NbLayoutModule, NbSidebarModule, NbSidebarService, NbIconComponent,
-   NbIconModule, NbButtonModule, NbSpinnerModule, NbDialogModule, NbDialogService,
-    NbTabsetModule, NbTooltipModule, NbListModule, NbSelectModule, NbInputModule} from '@nebular/theme';
+import {
+  NbCardModule, NbLayoutModule, NbSidebarModule, NbSidebarService, NbIconComponent,
+  NbIconModule, NbButtonModule, NbSpinnerModule, NbDialogModule, NbDialogService,
+  NbTabsetModule, NbTooltipModule, NbListModule, NbSelectModule, NbInputModule, NbAutocompleteModule
+} from '@nebular/theme';
 import {RouterModule} from '@angular/router';
 import {ComponentsModule} from '../../../components/components.module';
 import { FormsModule } from '@angular/forms';
+import {LayoutsModule} from '../../../layouts/layouts.module';
+
 
 
 
 @NgModule({
   declarations: [ClusterReleasesComponent],
   imports: [
+    LayoutsModule,
     CommonModule,
     RouterModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
     NbLayoutModule,
     NbSidebarModule, // NbSidebarModule.forRoot(), //if this is your app.module
     NbCardModule,
+    NbAutocompleteModule,
     ComponentsModule,
     NbSidebarModule,
     NbIconModule,
