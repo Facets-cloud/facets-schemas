26ffdaf56b2d37c6fa676fa68c5f51d2148e1909
Madan Gudi <madan.gopal@capillarytech.com>
2021-03-19 09:03:00 +0530

    helper changes

--- v2/cc-integration-tests/src/test/java/com/capillary/ops/cp/helpers/k8s/EksHelper.java
+++ v2/cc-integration-tests/src/test/java/com/capillary/ops/cp/helpers/k8s/EksHelper.java
@@ -1,17 +1,5 @@
 package com.capillary.ops.cp.helpers.k8s;
 
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
-//import com.amazonaws.auth.BasicSessionCredentials;
-//import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-//import com.amazonaws.services.eks.AmazonEKS;
-//import com.amazonaws.services.eks.AmazonEKSClientBuilder;
-//import com.amazonaws.services.eks.model.DescribeClusterRequest;
-//import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
-//import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
-//import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
-//import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
-//import com.amazonaws.services.securitytoken.model.Credentials;
-
 import com.capillary.ops.cp.bo.K8sConfig;
 import com.capillary.ops.cp.helpers.AwsCommonUtils;
 import com.capillary.ops.cp.helpers.CommonUtils;
@@ -48,47 +36,22 @@ public class EksHelper implements K8sHelper {
         JsonObject cluster = deploymentContextJson.getAsJsonObject("cluster");
         K8sConfig k8sConfig = new K8sConfig();
 
-        String roleARN = cluster.get("roleARN").getAsString();
-        String externalId = cluster.get("externalId").getAsString();
         String awsRegion = cluster.get("awsRegion").getAsString();
         String k8sClusterName = cluster.get("name").getAsString() + "-k8s-cluster";
 
-//        AWSSecurityTokenService stsClient = AWSSecurityTokenServiceClientBuilder.standard()
-//                .withCredentials(new ProfileCredentialsProvider())
-//                .withRegion(awsRegion)
-//                .build();
-//
-//        AssumeRoleRequest roleRequest = new AssumeRoleRequest()
-//                .withDurationSeconds(3600)
-//                .withRoleArn(roleARN)
-//                .withExternalId(externalId)
-//                .withRoleSessionName("integration-test-session");
-//        AssumeRoleResult roleResponse = stsClient.assumeRole(roleRequest);
-//        Credentials sessionCredentials = roleResponse.getCredentials();
-//
-//        BasicSessionCredentials awsCredentials = new BasicSessionCredentials(
-//                sessionCredentials.getAccessKeyId(),
-//                sessionCredentials.getSecretAccessKey(),
-//                sessionCredentials.getSessionToken());
-//
-//        AmazonEKS eks = AmazonEKSClientBuilder.standard()
-//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-//                .withRegion(awsRegion)
-//                .build();
+        EksClient eksClient = EksClient.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(awsCommonUtils.getSTSCredentialsProvider())
+                .build();
 
-        EksClient eksClient = EksClient.builder().region(Region.of(awsRegion)).credentialsProvider(awsCommonUtils.getSTSCredentialsProvider()).build();
+        DescribeClusterRequest request = DescribeClusterRequest
+                .builder()
+                .name(k8sClusterName)
+                .build();
 
-//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-//                .withRegion(awsRegion)
-//                .build();
-
-        DescribeClusterRequest request = DescribeClusterRequest.builder().name(k8sClusterName).build();
-
-        Cluster cluster1 = eksClient.describeCluster(request).cluster();
-        k8sConfig.setKubernetesApiEndpoint(cluster1.endpoint());
-        k8sConfig.setKubernetesToken(cluster1.clientRequestToken());
-//        k8sConfig.setKubernetesApiEndpoint(eksClient.describeCluster(request).getCluster().getEndpoint());
-//        k8sConfig.setKubernetesToken(eksClient.describeCluster(request).getCluster().getClientRequestToken());
+        Cluster eksCluster = eksClient.describeCluster(request).cluster();
+        k8sConfig.setKubernetesApiEndpoint(eksCluster.endpoint());
+        k8sConfig.setKubernetesToken(eksCluster.clientRequestToken());
 
         return k8sConfig;
     }
