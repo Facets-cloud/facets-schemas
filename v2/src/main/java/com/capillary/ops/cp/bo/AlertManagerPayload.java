68e4752eece4a12e2de56d370807cea7d376e7ad
Anshul Sao <anshul.sao@capillarytech.com>
2021-03-28 10:31:44 +0530

    Lombok is not working in inner classes

--- v2/src/main/java/com/capillary/ops/cp/bo/AlertManagerPayload.java
+++ v2/src/main/java/com/capillary/ops/cp/bo/AlertManagerPayload.java
@@ -12,7 +12,6 @@ public class AlertManagerPayload {
     public static final String ALERTNAME = "alertname";
 
 
-    @Getter
     public static class Alert {
         private String status;
         private HashMap<String, String> labels;
@@ -44,9 +43,56 @@ public class AlertManagerPayload {
                 return labels.get("severity");
             return "No Severity";
         }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public HashMap<String, String> getLabels() {
+            return labels;
+        }
+
+        public void setLabels(HashMap<String, String> labels) {
+            this.labels = labels;
+        }
+
+        public HashMap<String, String> getAnnotations() {
+            return annotations;
+        }
+
+        public void setAnnotations(HashMap<String, String> annotations) {
+            this.annotations = annotations;
+        }
+
+        public String getStartsAt() {
+            return startsAt;
+        }
+
+        public void setStartsAt(String startsAt) {
+            this.startsAt = startsAt;
+        }
+
+        public String getEndsAt() {
+            return endsAt;
+        }
+
+        public void setEndsAt(String endsAt) {
+            this.endsAt = endsAt;
+        }
+
+        public String getGeneratorURL() {
+            return generatorURL;
+        }
+
+        public void setGeneratorURL(String generatorURL) {
+            this.generatorURL = generatorURL;
+        }
     }
 
-    @Getter
     public static class Response {
         private String version;
         private String groupKey;
@@ -56,5 +102,69 @@ public class AlertManagerPayload {
         private HashMap<String, String> groupLabels;
         private String externalURL;
         private List<Alert> alerts;
+
+        public String getVersion() {
+            return version;
+        }
+
+        public void setVersion(String version) {
+            this.version = version;
+        }
+
+        public String getGroupKey() {
+            return groupKey;
+        }
+
+        public void setGroupKey(String groupKey) {
+            this.groupKey = groupKey;
+        }
+
+        public int getTruncatedAlerts() {
+            return truncatedAlerts;
+        }
+
+        public void setTruncatedAlerts(int truncatedAlerts) {
+            this.truncatedAlerts = truncatedAlerts;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getReceiver() {
+            return receiver;
+        }
+
+        public void setReceiver(String receiver) {
+            this.receiver = receiver;
+        }
+
+        public HashMap<String, String> getGroupLabels() {
+            return groupLabels;
+        }
+
+        public void setGroupLabels(HashMap<String, String> groupLabels) {
+            this.groupLabels = groupLabels;
+        }
+
+        public String getExternalURL() {
+            return externalURL;
+        }
+
+        public void setExternalURL(String externalURL) {
+            this.externalURL = externalURL;
+        }
+
+        public List<Alert> getAlerts() {
+            return alerts;
+        }
+
+        public void setAlerts(List<Alert> alerts) {
+            this.alerts = alerts;
+        }
     }
 }
