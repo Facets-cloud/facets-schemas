ccf982466681752b09e97def44856a6a0b1e51f9
Madan Gudi <madan.gopal@capillarytech.com>
2021-07-23 15:00:35 +0530

    fix

--- ops/wetty-ssh-host/startup.sh
+++ ops/wetty-ssh-host/startup.sh
@@ -2,9 +2,9 @@
 
 touch /tmp/startup
 echo "Init"
-setfacl -m u:capk8s:---  /bin/gunzip
-setfacl -m u:capk8s:---  /bin/gzip
-setfacl -m u:capk8s:---  /bin/uncompress
-setfacl -m u:capk8s:---  /bin/setfacl
+#setfacl -m u:capk8s:---  /bin/gunzip
+#setfacl -m u:capk8s:---  /bin/gzip
+#setfacl -m u:capk8s:---  /bin/uncompress
+#setfacl -m u:capk8s:---  /bin/setfacl
 
 /usr/sbin/sshd -D -e
